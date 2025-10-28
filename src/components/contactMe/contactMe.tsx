5127848b8c6b2205fd975657036b56ded2f62d7a
myfirst-demo <suhailullkhan619@gmail.com>
2025-10-28 21:32:46 +0530

    added links for contact

--- src/components/contactMe/contactMe.tsx
+++ src/components/contactMe/contactMe.tsx
@@ -2,75 +2,97 @@ import "./contactMe.css";
 import { Mail } from "lucide-react";
 import { Github } from "lucide-react";
 import { Linkedin } from "lucide-react";
-import { Twitter } from "lucide-react";
+import { CircleUserRound } from "lucide-react";
 const ContactMe = () => {
   return (
     <section>
-    <div className="contactMaindiv">
-      <div
-        data-aos="fade-down"
-        className="text-[28px] font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl "
-      >
-        Connect with me
-      </div>
-
-      <div className="connectContentDiv">
-        <div className="btndiv">
-          <button
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            className="connectbtn"
-          >
-            <span className="logo">
-              <Mail size={30} />
-            </span>
-            Email
-          </button>
-
-          <button
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            className="connectbtn"
-          >
-            <span className="logo">
-              {" "}
-              <Linkedin size={30} />
-            </span>
-            linkedIn
-          </button>
+      <div className="contactMaindiv">
+        <div
+          data-aos="fade-down"
+          className="text-[28px] font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl "
+        >
+          Connect with me
         </div>
 
-        <div className="btndiv">
-          <button
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            className="connectbtn"
-          >
-            <span className="logo">
-              {" "}
-              <Github size={30} />
-            </span>
-            Github
-          </button>
+        <div className="connectContentDiv">
+          <div className="btndiv">
+            <a
+              href="mailto:suhailullakhan619@gmail.com"
+              target="blank"
+              className="link"
+            >
+              <button
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                className="connectbtn"
+              >
+                <span className="logo">
+                  <Mail size={30} />
+                </span>
+                Email
+              </button>
+            </a>
 
-          <button
-            data-aos="flip-left"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
-            className="connectbtn"
-          >
-            <span className="logo">
-              <Twitter size={30} />
-            </span>
-            Twitter
-          </button>
+            <a
+              href="https://www.linkedin.com/in/suhailullakhan"
+              target="blank"
+              className="link"
+            >
+              <button
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                className="connectbtn"
+              >
+                <span className="logo">
+                  <Linkedin size={30} />
+                </span>
+                linkedIn
+              </button>
+            </a>
+          </div>
+
+          <div className="btndiv">
+            <a
+              href="https://github.com/suhailullakhan619"
+              target="blank"
+              className="link"
+            >
+              <button
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                className="connectbtn"
+              >
+                <span className="logo">
+                  {" "}
+                  <Github size={30} />
+                </span>
+                Github
+              </button>
+            </a>
+            <a
+              href="https://wa.me/917204338475"
+              target="blank"
+              className="link"
+            >
+              <button
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                className="connectbtn"
+              >
+                <span className="logo">
+                  <CircleUserRound size={30} />
+                </span>
+                Whatsapp
+              </button>
+            </a>
+          </div>
         </div>
+        <hr style={{ width: "100%" }} />
       </div>
-      <hr style={{width:'100%'}} />
-    </div>
     </section>
   );
 };
