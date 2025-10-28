53ef9f1f654abffb65a699a4043ea8c645b28e68
myfirst-demo <suhailullkhan619@gmail.com>
2025-10-28 14:33:45 +0530

    added project section

--- src/components/projectsSection/projects.tsx
+++ src/components/projectsSection/projects.tsx
@@ -12,10 +12,70 @@ const Projects = () => {
                 <div data-aos="zoom-in" className='aboutProjects'>
                     
                       <div className='ProjectsDes'>
+
+                        <div className='flex flex-col gap-[15px]'>
+                          <p  className='text-[17px] font-extrabold'>Internship Project </p>
                         <div className='contentdiv'>
-                          <div>🚀</div>
-                          <p><span className='text-[17px] font-semibold'>Developed and deployed</span> responsive web applications using <span className='text-[17px] font-semibold' > HTML, CSS, JavaScript, React, and Tailwind CSS.</span>
+                          <div>🔗</div>
+                          <p><a href="https://github.com/suhailullakhan619/Sports-Shop" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>Sports Shop App -</span></a> Internship project integrating backend APIs with React frontend for product and order management.
                       </p>
+                        </div>
+                        </div>
+
+
+                        <div className='flex flex-col gap-[15px]'>
+                          <p  className='text-[17px] font-extrabold'>Personal Project </p>
+                         <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="https://e-com-website-frontend.vercel.app/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>E-Commerce Website -</span></a> Developed a feature-rich React-based e-commerce platform with pagination and API integration for optimized performance and scalability</p>
+                        </div>
+
+
+                          <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="https://suhailchatbox.netlify.app/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>Chatbot App -</span></a> React-based conversational UI with local storage message persistence.
+                      </p>
+                        </div>
+
+                        <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="http://suhailvisdumsite.netlify.app" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>Visdum Site  -</span></a> Static React landing page showcasing responsive design techniques.
+                      </p>
+                        </div>
+
+                        <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href=" http://suhailpsswebsite.netlify.app" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>PSS-Site -</span></a> Educational web page project highlighting structured layouts and animations.
+                      </p>
+                        </div>
+
+                           <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="https://suhailullakhan619.github.io/Pw-Website/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>PW Website -</span></a> Fully crafted using Tailwind CSS, showcasing responsive design, clean UI, and modern visual consistency across all devices.
+                      </p>
+                        </div>
+
+                        <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href=" https://suhailullakhan619.github.io/YouTube-UI/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>YouTube UI  -</span></a> Responsive interface replicating modern video platform design using plain HTML and CSS.
+                      </p>
+                        </div>
+
+                        
+                        <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="https://suhailtodo-list.netlify.app/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>To-Do List App  -</span></a> Simple yet efficient task manager demonstrating CRUD operations.
+                      </p>
+                        </div>
+
+                        <div className='contentdiv'>
+                          <div>🔗</div>
+                          <p><a href="https://suhailullakhan619.github.io/Simple-Calculator/" target="_blank" rel="noopener noreferrer"><span className='text-[17px] font-semibold border-b-[2px] border-[#011D56] py-[12px] cursor-pointer'>Calculator App  -</span></a> Interactive calculator built using HTML, CSS, and JavaScript.
+                      </p>
+                        </div>
+
+
+
                         </div>
                       
 
